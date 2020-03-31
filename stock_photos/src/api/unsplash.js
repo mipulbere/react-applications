b536b0cdb8114f44a90a846cdef15835b8b198e5
Mirela Pulbere <mipulbere@gmail.com>
2020-03-31 12:13:39 -0400

    hid keys

--- stock_photos/src/api/unsplash.js
+++ stock_photos/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 7Ln7UMubH9cLScVpul4ngSYFK-JYbo9V5r2pnPudrzw'
+        Authorization: 'Insert key here'
       }
 });
--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = 'AIzaSyBDFIBxc3aL-yKjnPjqI6iyUvaE47TFYqY';
+const KEY = 'Insert your api key here';
  
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3", 
